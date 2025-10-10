e0aafbb6cc3115ed0843b328d060a2ec0956239c
Gabrielle Oliveira de Paula <gabrielleoliveirah14@gmail.com>
2025-10-10 15:50:29 -0300

    Update create_tables.sql

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,16 @@
+CREATE TABLE usuarios (
+    id_usuario NUMBER PRIMARY KEY,
+    nome VARCHAR2(100) NOT NULL,
+    email VARCHAR2(100) NOT NULL
+);
+
+CREATE TABLE tarefas (
+    id_tarefa NUMBER PRIMARY KEY,
+    titulo VARCHAR2(100) NOT NULL,
+    descricao VARCHAR2(255),
+    status VARCHAR2(50),
+    responsavel NUMBER,
+    CONSTRAINT fk_responsavel FOREIGN KEY (responsavel) REFERENCES usuarios(id_usuario)
+);
+
 
