fd291def33ee593c722e89bb40401a0d5e65bb90
Gabrielle Oliveira de Paula <gabrielleoliveirah14@gmail.com>
2025-10-10 15:50:59 -0300

    Update insert_usuarios.sql

--- sql/insert_usuarios.sql
+++ sql/insert_usuarios.sql
@@ -1,1 +1,6 @@
+INSERT INTO usuarios (id_usuario, nome, email) VALUES (1, 'Gabrielle Oliveira', 'gabrielle@email.com');
+INSERT INTO usuarios (id_usuario, nome, email) VALUES (2, 'Adrielly Silva', 'adrielly@email.com');
+INSERT INTO usuarios (id_usuario, nome, email) VALUES (3, 'Luísa Costa', 'luisa@email.com');
+COMMIT;
+
 
