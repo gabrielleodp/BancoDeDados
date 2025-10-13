944de3efdf22c99298859039f96cbbdd8415abf0
Luísa Vernersbach Varejão <luisavvarejao@hotmail.com>
2025-10-12 22:44:17 -0300

    Update insert_usuarios.sql

--- sql/insert_usuarios.sql
+++ sql/insert_usuarios.sql
@@ -1,6 +1,7 @@
-INSERT INTO usuarios (id_usuario, nome, email) VALUES (1, 'Gabrielle Oliveira', 'gabrielle@email.com');
-INSERT INTO usuarios (id_usuario, nome, email) VALUES (2, 'Adrielly Silva', 'adrielly@email.com');
-INSERT INTO usuarios (id_usuario, nome, email) VALUES (3, 'Luísa Costa', 'luisa@email.com');
+INSERT INTO USUARIOS (id_usuario, nome, email) VALUES (1, 'Gabrielle Oliveira', 'gabrielle@email.com');
+INSERT INTO USUARIOS (id_usuario, nome, email) VALUES (2, 'Adrielly Costa', 'adrielly@email.com');
+INSERT INTO USUARIOS (id_usuario, nome, email) VALUES (3, 'Luísa Varejão', 'luisa@email.com');
 COMMIT;
+
 
 
