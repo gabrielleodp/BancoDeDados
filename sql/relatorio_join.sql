bb2863a361a03b83ada64990437e161555dd3d61
Luísa Vernersbach Varejão <luisavvarejao@hotmail.com>
2025-10-12 22:46:48 -0300

    Update relatorio_join.sql

--- sql/relatorio_join.sql
+++ sql/relatorio_join.sql
@@ -1,1 +1,6 @@
+-- Consulta para retornar quantidade de tarefas relacionadas a cada usuário. 
+
+SELECT t.id_tarefa, t.titulo, t.status, u.nome
+FROM tarefas t
+JOIN usuarios u ON t.usuario_tarefa = u.id_usuario;
 
