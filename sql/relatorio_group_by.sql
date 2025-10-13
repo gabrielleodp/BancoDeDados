4c50b86f90da741c7c0d995abed6b62099615058
Luísa Vernersbach Varejão <luisavvarejao@hotmail.com>
2025-10-12 22:45:35 -0300

    Update relatorio_group_by.sql

--- sql/relatorio_group_by.sql
+++ sql/relatorio_group_by.sql
@@ -1,1 +1,6 @@
+-- Consulta para retornar o total de tarefas cadastradas na base e o nome dos usuários que esão executando as tarefas
+
+SELECT usuario_tarefa, COUNT(*) AS total_tarefas
+FROM tarefas
+GROUP BY usuario_tarefa;
 
