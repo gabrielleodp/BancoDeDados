1bff59fed2570a19444a8a295a3cad20b482a227
Luísa Vernersbach Varejão <luisavvarejao@hotmail.com>
2025-10-12 22:43:45 -0300

    Update insert_tarefas.sql

--- sql/insert_tarefas.sql
+++ sql/insert_tarefas.sql
@@ -1,1 +1,5 @@
+INSERT INTO tarefas (id_tarefa, titulo, descricao, status, usuario_tarefa) VALUES (1, 'Estudar SQL', 'Estudar comandos básicos de SQL', 'Pendente', 1);
+INSERT INTO tarefas (id_tarefa, titulo, descricao, status, usuario_tarefa) VALUES (2, 'Criar classes', 'Criar model e controller', 'Em andamento', 2);
+INSERT INTO tarefas (id_tarefa, titulo, descricao, status, usuario_tarefa) VALUES (3, 'Testar sistema', 'Testar todas as funcionalidades', 'Pendente', 3);
+COMMIT;
 
