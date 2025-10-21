18f8b1c49964591d11dbaf2eff86907ba8f37abd
Adrielly Costa <adriellycosta2108@hotmail.com>
2025-10-21 13:07:39 -0300

    Banco de dados
    Minha parte, banco de dados

---
+++ src/utils/config.py
@@ -0,0 +1,28 @@
+MENUS = {
+    "principal": """
+Sistema de Gerenciamento de Tarefas
+1 - Gerenciar Usuários
+2 - Gerenciar Tarefas
+3 - Relatórios
+0 - Sair
+""",
+    "usuarios": """
+1 - Inserir Usuário
+2 - Atualizar Usuário
+3 - Excluir Usuário
+4 - Listar Usuários
+0 - Voltar
+""",
+    "tarefas": """
+1 - Inserir Tarefa
+2 - Atualizar Tarefa
+3 - Excluir Tarefa
+4 - Listar Tarefas
+0 - Voltar
+""",
+    "relatorios": """
+1 - Tarefas por Usuário (GROUP BY)
+2 - Tarefas com JOIN
+0 - Voltar
+"""
+}
---
+++ src/utils/splash_screen.py
@@ -0,0 +1,8 @@
+def mostrar_splash():
+    print("===================================")
+    print(" Sistema de Gerenciamento de Tarefas ")
+    print(" Grupo: Gabrielle, Adrielly, Luísa ")
+    print(" Professor: Howard Cruz Roatti ")
+    print(" Disciplina: Banco de Dados ")
+    print(" Semestre: 2025/2 ")
+    print("===================================")
